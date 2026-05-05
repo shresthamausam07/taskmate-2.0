3e4fc7e6c6c1399bf7b6ab217576d483c21d9eda
Mausam Shrestha <shresthamausam12345@gmail.com>
2026-05-04 22:52:14 -0400

    use env var for api url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5001', { autoConnect: false });
+const socket = io(import.meta.env.VITE_API_URL || 'http://localhost:5001', { autoConnect: false });
 export default socket;
